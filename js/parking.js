57f96eeef6825d176621d1a7939ff0bee77ed8a6
laumales <cruzlauramales@gmail.com>
2026-04-19 11:12:03 -0500

    se crean funciones en js para los bontones del index

---
+++ js/parking.js
@@ -0,0 +1,12 @@
+function RegistrodeEntrada() {
+    window.location.href = 'entradas.html';
+}
+
+function RegistrDeSalida() {
+    window.location.href = 'salidas.html';
+}
+
+function VerPaqueadero(){
+    // Aquí puedes agregar lógica para mostrar el parqueadero, por ejemplo, redirigir a otra página o mostrar un modal
+    alert('Función para ver parqueadero no implementada aún. :v');
+}
