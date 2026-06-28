0a73ed22f3d507d2e7bf01c5fcb792e49509ae85
laumales <cruzlauramales@gmail.com>
2026-06-28 18:24:31 -0500

    se añade nueva función y se empieza a estructurar con javascript para algunas funciones

--- js/logica-index.js
+++ js/logica-index.js
@@ -1,3 +1,4 @@
+ // Lógica para mostrar los registros en la tabla
 fetch("../data/registro.json")
     .then(respuesta => respuesta.json())
     .then(registros => {
@@ -17,3 +18,40 @@ fetch("../data/registro.json")
         });
 
     })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// lógica para la mostrar la modal o esconderla
+const btnBarcode = document.getElementById("btn-barcode");
+const modal = document.getElementById("IDmodal"); 
+
+btnBarcode.addEventListener("click", () => {
+    modal.style.display = "flex";
+})
